264926059406c4cfad2db1db2dffe21e42e48b44
eliottness <eliottmini@bouhana.com>
2020-07-08 17:07:43 +0200

    started db support(sqlite3)

---
+++ episcan/database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+DB_FILE = NONE
+
+def init(db_file):
+    global DB_FILE = db_file
+
+def get_all_mangas():
+    """
+    format all the table manga_list into a list of dict
+    """
+    pass
+
+def get_mangas_list():
+    """
+    get all the manga filenames in the db
+    """
+    pass
+
+def update_manga(manga):
+    """
+    arg: a manga object
+    """
+    pass
+
+def add_manga(manga):
+    """
+    arg1: a manga object
+    """
+    pass
